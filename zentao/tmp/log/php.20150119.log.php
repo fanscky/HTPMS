3de67445128245efe74aaee8b6361a8a74618c0e
fanscky <fanscky@163.com>
2015-01-19 17:45:56 +0800

    [2015-1-19]
    增加调试手段：
    
    【1】配置Xdebug用于zend studio调试（前提是将xdebug库文件php_xdebug_xxxx.dll放在php安装目录的ext文件夹下）
         /php/php.ini
         /runner/res/php/php.ini
         /php/php.raw.ini
         三个文件中末尾均新增以下行
    
         [XDebug]
         ;调试功能模块儿
         zend_extension ="f:/xampp/php/ext/php_xdebug.dll"
         xdebug.remote_handler=dbgp
         ;开启远程调试
         xdebug.remote_enable = On
         ;远程主机
         xdebug.remote_host=localhost
         ;主机端口
         xdebug.remote_port=9000
         ;开启自动跟踪
         xdebug.auto_trace = On
         ;开启异常跟踪
         xdebug.show_exception_trace = On
         ;开启远程调试自动启动
         xdebug.remote_autostart = Off
         ;收集变量
         xdebug.collect_vars = On
         ;收集返回值
         xdebug.collect_return = On
         ;收集参数
         xdebug.collect_params = On
         ;临时跟踪信息输出
         xdebug.trace_output_dir="f:\xampp\php\temp\"
         ;显示局部变量
         xdebug.show_local_vars = On
         xdebug.profiler_enable = On
         xdebug.profiler_output_dir ="f:\xampp\php\temp\"
         xdebug.trace_enable_trigger =On
    
    【2】配置firephp库文件用于Firefox调用firephp调试（前提是将firephp库文件FirePHPCore整个目录放在服务器中）
         /php/php.ini
         /runner/res/php/php.ini
         /php/php.raw.ini
         中“auto_prepend_file = ”修订变量为 auto_prepend_file = "f:\xampp\FirePHPCore\fb.php"

---
+++ zentao/tmp/log/php.20150119.log.php
@@ -0,0 +1,88 @@
+<?php
+ die();
+?>
+
+11:10:13 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2324_3_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 11:10:13',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+11:14:44 Call to undefined function fb() in F:\xampp\zentao\module\todo\model.php on line 15 when visiting my-todo
+
+11:14:57 Call to undefined function fb() in F:\xampp\zentao\module\todo\model.php on line 15 when visiting my-todo
+
+11:15:34 Call to undefined function fb() in F:\xampp\zentao\module\todo\model.php on line 15 when visiting my
+
+11:22:17 syntax error, unexpected 'fb' (T_STRING) in F:\xampp\zentao\module\todo\model.php on line 48 when visiting todo-create-20150119
+
+11:22:21 syntax error, unexpected 'fb' (T_STRING) in F:\xampp\zentao\module\todo\model.php on line 48 when visiting my-todo
+
+11:25:32 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting todo-create-20150119
+
+11:25:37 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:25:54 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:25:55 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:09 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my
+
+11:26:12 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:14 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:16 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:45 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:47 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:48 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:49 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:56 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:57 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my-todo
+
+11:26:59 Cannot redeclare fb() (previously declared in F:\xampp\FirePHPCore\fb.php:61) in F:\xampp\zentao\module\todo\FirePHPCore\fb.php on line 65 when visiting my
+
+13:50:49 syntax error, unexpected 'fb' (T_STRING), expecting function (T_FUNCTION) in F:\xampp\zentao\module\todo\model.php on line 351 when visiting my-todo
+
+13:51:58 syntax error, unexpected 'FB' (T_STRING), expecting function (T_FUNCTION) in F:\xampp\zentao\module\todo\model.php on line 350 when visiting my-todo
+
+13:52:33 syntax error, unexpected '
+13:53:08 syntax error, unexpected 'return' (T_RETURN) in F:\xampp\zentao\module\todo\model.php on line 51 when visiting my-todo
+
+13:53:19 syntax error, unexpected 'return' (T_RETURN) in F:\xampp\zentao\module\todo\model.php on line 51 when visiting my-todo
+
+13:53:50 syntax error, unexpected 'return' (T_RETURN) in F:\xampp\zentao\module\todo\model.php on line 51 when visiting my-todo
+
+13:54:52 include_once(./FirePHPCore/fb.php): failed to open stream: No such file or directory in F:\xampp\zentao\module\todo\model.php on line 14 when visiting my-todo
+
+13:54:52 include_once(): Failed opening './FirePHPCore/fb.php' for inclusion (include_path='.;F:\xampp\php/PEAR') in F:\xampp\zentao\module\todo\model.php on line 14 when visiting my-todo
+
+14:27:20 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql25d8_5_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 14:27:20',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+14:36:13 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_7_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_todo` SET `date` = '2015-01-19',`type` = 'bug',`pri` = '3',`name` = '',`desc` = '',`status` = 'doing',`begin` = '1440',`end` = '1510',`account` = 'caikaiye',`idvalue` = '3' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting todo-create-20150119
+
+14:36:27 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_8_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_todo` SET `date` = '2015-01-19',`type` = 'bug',`pri` = '3',`name` = '',`desc` = '',`status` = 'doing',`begin` = '1440',`end` = '1510',`account` = 'caikaiye',`idvalue` = '3' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting todo-create-20150119
+
+14:46:50 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_27_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 14:46:50',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+14:47:49 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_2f_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 14:47:49',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+14:50:17 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_40_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 14:50:17',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+15:20:20 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_6b_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 15:20:20',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+15:38:32 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql2cc8_90_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 15:38:32',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
+
+16:34:10 ERROR: SQLSTATE[HY000]: General error: 1 Can't create/write to file 'F:\xampp\mysql\tmp\#sql21e8_3_0.MYI' (Errcode: 2)The sql is: INSERT INTO `zt_action` SET `objectType` = 'user',`objectID` = '2',`actor` = 'caikaiye',`action` = 'login',`date` = '2015-01-19 16:34:10',`comment` = '',`extra` = '',`product` = ',0,',`project` = '0' in F:\xampp\zentao\lib\dao\dao.class.php on line 1058, last called by F:\xampp\zentao\lib\dao\dao.class.php on line 992 through function sqlError.
+ in F:\xampp\zentao\framework\router.class.php on line 1589 when visiting user-login-L3plbnRhby8=
